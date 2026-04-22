0889418add7c57f83df0cb5d96ae4c72273f2dbf
hoiregeno <hoiregeno75@gmail.com>
2026-04-22 20:37:27 +1000

    style: style navigation bar, logo and list items of links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,20 @@ const NAVLINKS = [
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Website</Link>
-      <ul>
+    <nav className="bg-teal-900 flex justify-between text-teal-200 px-10 py-2">
+      <Link to="/" className="self-center font-extrabold">
+        Website
+      </Link>
+
+      <ul className="flex items-center font-semibold gap-6">
         {NAVLINKS.map(({ id, label, to }) => (
           <li key={id}>
-            <NavLink to={to}>{label}</NavLink>
+            <NavLink
+              to={to}
+              className="flex py-1 px-4 hover:bg-teal-200/10 rounded-md transition-colors duration-150 ease-in"
+            >
+              {label}
+            </NavLink>
           </li>
         ))}
       </ul>
